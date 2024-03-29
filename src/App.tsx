2ad083e7c1abe541d53feb886a937ce5b68d9c75
codesan-git <satrianugrahacode@gmail.com>
2024-03-30 05:55:27 +0700

    boiler plate of react and sass

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/index.scss'
+function App() {
+  return (
+    <div className='hello-scss'>
+      Hello React With Vite and SCSS
+      <div className='sub-hello-text'>Sample Text</div>
+    </div>
+  )
+}
+
+export default App
